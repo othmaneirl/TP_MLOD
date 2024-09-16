5de9342e41056afb705331881d984f4cf2c2a492
othmaneirl <121831157+othmaneirl@users.noreply.github.com>
2024-09-16 10:00:20 +0200

    Fini question c exo dates

--- TP-02-pointeurs/03-Dates/dates.c
+++ TP-02-pointeurs/03-Dates/dates.c
@@ -3,23 +3,22 @@
 #include <stdint.h>
 #include "dates.h"
 
-
-
 void initialiseDate(struct Date *date) {
     printf("Entrer le jour : ");
     scanf("%d", &date->jour);
-
-    printf("Entrer l'annee : ");
-    scanf("%d", &date->annee);
-
+    
     int mois_entre;
     printf("Entrer le mois (1-12) : ");
     scanf("%d", &mois_entre);
     date->mois = (enum Mois)mois_entre;
 
+    printf("Entrer l'annee : ");
+    scanf("%d", &date->annee);
+    
+
 }
 
-void afficherDate(struct Date *date) {
+void afficheDate(struct Date *date) {
     printf("Date : %d/%d/%d\n", date->jour, date->mois, date->annee);
 }
 
